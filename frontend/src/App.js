acbe77845b96431fa4e5cce17be50b4e0d4b13ce
vitorvd <vitordversiani@gmail.com>
2020-03-28 13:05:00 -0300

    Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './global.css';
+
+//vai procurar pelo arquivo index.js
+import Routes from './routes';
+
+// JSX(JavaScript XML)
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
